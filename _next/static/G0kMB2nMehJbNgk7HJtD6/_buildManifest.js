8802a108d370aa69854a5cecb55d9fa25a9142a1
Adriel Bernhard <adrielbth01@gmail.com>
2026-01-09 17:49:01 +0700

    Updates

--- _next/static/G0kMB2nMehJbNgk7HJtD6/_buildManifest.js
+++ _next/static/G0kMB2nMehJbNgk7HJtD6/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Portofolio-ABT//_next/:path+",
+        "destination": "/Portofolio-ABT/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
