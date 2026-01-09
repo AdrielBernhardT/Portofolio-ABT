1b753d4773fc4c2bafc057b2ab4ae88c712301e6
Adriel Bernhard <adrielbth01@gmail.com>
2026-01-09 17:40:59 +0700

    Upload pertama Portofolio Website

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/Portofolio-Website', 
+};
+
+export default nextConfig;
