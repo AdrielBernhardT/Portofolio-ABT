e9d1bcadd6f5a91e3839135eafeb2a2fdbdfb688
Adriel Bernhard <adrielbth01@gmail.com>
2026-01-19 13:50:59 +0700

    Update Projects

--- app/page.tsx
+++ app/page.tsx
@@ -470,7 +470,7 @@ export default function Home() {
           ].map((project, index) => (
             <div
               key={index}
-              className="shrink-0 min-w-[85%] sm:min-w-[400px] md:min-w-[450px] lg:min-w-[500px] snap-center bg-[#111] rounded-2xl border border-gray-800 hover:border-blue-500/50 transition-all duration-500 group flex flex-col overflow-hidden h-full shadow-lg"
+              className="min-w-[85%] sm:min-w-[400px] md:min-w-[450px] lg:min-w-[500px] snap-center bg-[#111] rounded-2xl border border-gray-800 hover:border-blue-500/50 transition-all duration-500 group flex flex-col overflow-hidden h-full shadow-lg"
             >
               {/* Bagian 1: Image */}
               <div className="h-44 sm:h-48 w-full bg-gray-900 flex items-center justify-center relative overflow-hidden">
